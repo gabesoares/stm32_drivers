3e26a2ea77012b2655e85d057230eaf58ce82132
gabesoares <gsoares026@gmail.com>
2019-07-01 10:06:36 -0700

    Added Flash, ROM, SRAM1, SRAM2 base addresses

--- drivers/inc/stm32f407xx.h
+++ drivers/inc/stm32f407xx.h
@@ -8,6 +8,17 @@
 #ifndef INC_STM32F407XX_H_
 #define INC_STM32F407XX_H_
 
+/*
+ * base addresses of flash and SRAM memories
+ */
 
+// base addresses are from STM32F407xx reference manual
+// by default the values are considered signed, so we add U for unsigned int
+#define FLASH_BASEADDR 			0x08000000U // also called main memory
+#define SRAM1_BASEADDR 			0x20000000U
+#define SRAM2_BASEADDR			0x2001C000U // SRAM2 is the auxiliary internal SRAM
+#define ROM 					0x1FFF0000U // also called system memory
+
+#define SRAM 					SRAM1_BASEADDR /* SRAM1 acts as our main SRAM
 
 #endif /* INC_STM32F407XX_H_ */
