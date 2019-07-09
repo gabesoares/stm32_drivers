30782e655ed63850e8bb54a4fe9e4099707db9c7
gabesoares <gsoares026@gmail.com>
2019-07-08 18:53:33 -0700

    Created source and header files for SPI peripheral

---
+++ drivers/inc/stm32f407xx_spi_driver.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f407xx_spi_driver.h
+ *
+ *  Created on: Jul 8, 2019
+ *      Author: gabriel
+ */
+
+#ifndef INC_STM32F407XX_SPI_DRIVER_H_
+#define INC_STM32F407XX_SPI_DRIVER_H_
+
+#include "stm32f407xx.h"
+
+#endif /* INC_STM32F407XX_SPI_DRIVER_H_ */
---
+++ drivers/src/stm32f407xx_spi_driver.c
@@ -0,0 +1,8 @@
+/*
+ * stm32f407xx_spi_driver.c
+ *
+ *  Created on: Jul 8, 2019
+ *      Author: gabriel
+ */
+
+
